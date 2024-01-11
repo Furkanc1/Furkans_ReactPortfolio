48386972b28f5b922b7b06420beab75b9f5e5b55
Furkan Coskun <furkdurk7@aol.com>
2024-01-10 19:49:29 -0500

    got projects on portfolio page ( through array not api)

--- furksreactportfolio/src/components/portfolio/portfolio.js
+++ furksreactportfolio/src/components/portfolio/portfolio.js
@@ -1,30 +1,31 @@
 import "../../App.css";
-import React from 'react';
+import React from "react";
 
-export default function Portfolio({projects}) {
+export default function Portfolio({ projects }) {
   console.log("projects", projects);
   return (
     <div className="sectionContainer">
       <h2>My Projects:</h2>
+
       <div className="projectBoxContainer">
-        <div className={"project flex alignCenter justifyCenter flexColumn"}>
-          <h3>project 1</h3>
-          <p>
-            project description
-          </p>
-        </div>
-        <div className={"project flex alignCenter justifyCenter flexColumn"}>
-          <h3>project 2</h3>
-          <p>
-            project description
-          </p>
-        </div>
-        <div className={"project flex alignCenter justifyCenter flexColumn"}>
-          <h3>project 3</h3>
-          <p>
-            project description
-          </p> flexColumn
-        </div>
+        {projects && projects.length > 0 ? (
+          projects.map((project, projectIndex) => {
+            return (
+              <div key={projectIndex} className={"project flex alignCenter justifyCenter flexColumn"}>
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+                <a href={project.url}>
+                  <button className="projectLinkButton">Click Me!</button>
+                  </a>
+              </div>
+            );
+            
+          })
+        ) : (
+          <div className={"project flex alignCenter justifyCenter flexColumn"}>
+            No Projects Found {`:(`}
+          </div>
+        )}
       </div>
     </div>
   );
